9e5d9682edd7d0f25022580819b26bcc4f297da7
Xenox003 <xenox003yt@gmail.com>
2025-10-04 15:12:16 +0200

    Add playtime to player list

--- src/main/java/de/jxdev/legendarycraft/v3/playerlist/PlayerListComponents.java
+++ src/main/java/de/jxdev/legendarycraft/v3/playerlist/PlayerListComponents.java
@@ -1,11 +1,12 @@
 package de.jxdev.legendarycraft.v3.playerlist;
 
+import de.jxdev.legendarycraft.v3.LegendaryCraft;
+import de.jxdev.legendarycraft.v3.service.PlayerStatsService;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
-
-import java.time.format.TextStyle;
+import org.bukkit.entity.Player;
 
 public class PlayerListComponents {
     public static Component getPlayerListHeader() {
@@ -15,12 +16,19 @@ public class PlayerListComponents {
                 .append(Component.text(""));
     }
 
-    public static Component getPlayerListFooter() {
-        return Component.text("\n Aktuell " + Bukkit.getServer().getOnlinePlayers().size() + " online \n")
-                .append(Component.text(" TPS: ")
-                        .append(getTpsComponent())
-                        .append(Component.text(" "))
-                )
+    public static Component getPlayerListFooter(Player player) {
+        Component onlineLine = Component.text("\n Aktuell " + Bukkit.getServer().getOnlinePlayers().size() + " online \n");
+        Component tpsLine = Component.text(" TPS: ")
+                .append(getTpsComponent())
+                .append(Component.text(" "));
+        Component playtimeLine = Component.text("\n Spielzeit: ")
+                .append(Component.text(formatPlaytime(player)))
+                .append(Component.text(" \n"));
+
+        return Component.empty()
+                .append(onlineLine)
+                .append(tpsLine)
+                .append(playtimeLine)
                 .color(NamedTextColor.DARK_GRAY);
     }
 
@@ -30,7 +38,14 @@ public class PlayerListComponents {
         return Component.text(String.format("%.2f", tps), color);
     }
 
+    private static String formatPlaytime(Player player) {
+        long ms = LegendaryCraft.getInstance().getPlayerStatsService().getTotalPlaytimeMsIncludingActive(player.getUniqueId());
+        return PlayerStatsService.formatDuration(ms);
+    }
+
     public static void updateGlobalPlayerlist() {
-        Bukkit.getServer().sendPlayerListHeaderAndFooter(getPlayerListHeader(), getPlayerListFooter());
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.sendPlayerListHeaderAndFooter(getPlayerListHeader(), getPlayerListFooter(p));
+        }
     }
 }
